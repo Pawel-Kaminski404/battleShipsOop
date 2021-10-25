4749ed13a06f8546ab1cd6fc99616151f303df68
Dominik Berniak <berniak.dominik@gmail.com>
2021-10-25 16:16:33 +0200

    Implemented Board and Square classes

--- Battleships/Battleships/Board.cs
+++ Battleships/Battleships/Board.cs
@@ -2,6 +2,25 @@
 {
     public class Board
     {
+        public Square[,] Ocean { get; }
         
+        private const int BoardSize = 10;
+
+        public Board()
+        {
+            Ocean = new Square[BoardSize, BoardSize];
+            PopulateOcean();
+        }
+
+        private void PopulateOcean()
+        {
+            for (int i = 0; i < BoardSize; i++)
+            {
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    Ocean[i, j] = new Square(i, j);
+                }
+            }
+        }
     }
 }
--- Battleships/Battleships/Square.cs
+++ Battleships/Battleships/Square.cs
@@ -1,7 +1,46 @@
-﻿namespace Battleships
+﻿using System;
+using System.Collections.Generic;
+
+namespace Battleships
 {
     public class Square
     {
+        private (int, int) Position { get; }
         
+        public Enum SquareStatus { get; }
+
+        public Square(int x, int y)
+        {
+            SquareStatus = SquareStatuses.Empty;
+            Position = (x, y);
+        }
+
+        public char GetCharacter()
+        {
+            return this.SquareStatus switch
+            {
+                SquareStatuses.Empty => _squareChars["empty"],
+                SquareStatuses.Ship => _squareChars["ship"],
+                SquareStatuses.Hit => _squareChars["hit"],
+                SquareStatuses.Missed => _squareChars["missed"]
+            };
+        }
+
+        private readonly Dictionary<string, char> _squareChars = new()
+        {
+            {"empty", 'E'},
+            {"ship", 'S'},
+            {"hit", 'H'},
+            {"missed", 'M'}
+        };
+
+    }
+
+    public enum SquareStatuses
+    {
+        Empty,
+        Ship,
+        Hit,
+        Missed
     }
 }
